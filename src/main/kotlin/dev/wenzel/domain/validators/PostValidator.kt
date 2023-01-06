43fd086b6189fc66dc14b1fb790185c45b467cd9
Jose Wenzel <devwenzel@gmail.com>
2023-01-06 16:25:44 -0500

    refactor(post-validator): simplify validation logic

--- src/main/kotlin/dev/wenzel/domain/validators/PostValidator.kt
+++ src/main/kotlin/dev/wenzel/domain/validators/PostValidator.kt
@@ -1,9 +1,8 @@
 package dev.wenzel.domain.validators
 
+
 class PostValidator {
-    fun isValidSlug(slug: String): Boolean {
-        return !slug.contains(" ") && isKebabCase(slug)
-    }
+    fun isValidSlug(slug: String) = isKebabCase(slug)
 
     private fun isKebabCase(text: String) = text.matches(Regex("^([a-z][a-z0-9]*)(-[a-z0-9]+)*\$"))
 }
